e6c2154b1d4683e9aa94e3a5878af54cca0f9f0e
Samuel Ljevar <s_lj_91@yahoo.com>
2024-02-26 12:53:07 +0100

    mounting superdanko coming soon page

---
+++ app/ui/text-reveal-preview.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import {
+  TextRevealCard,
+  TextRevealCardDescription,
+  TextRevealCardTitle,
+} from "./text-reveal-card";
+
+export function TextRevealCardPreview() {
+  return (
+    <div className="flex text-center items-center justify-center bg-[#09090c] h-[40rem] rounded-2xl w-full">
+      <TextRevealCard
+        text="  Superdanko Shop"
+        revealText="We're launching soon... "
+      >
+        {/* <TextRevealCardTitle>
+          Sometimes, you just need to see it.
+        </TextRevealCardTitle> */}
+        <TextRevealCardDescription>
+           Hover over the card to reveal the hidden
+          text.
+        </TextRevealCardDescription>
+      </TextRevealCard>
+    </div>
+  );
+}
