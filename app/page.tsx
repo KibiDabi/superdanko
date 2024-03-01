646baa3952b0c5e65ce1b111504a5cea0efe40e1
Samuel Ljevar <s_lj_91@yahoo.com>
2024-03-01 15:04:10 +0100

    added Lottie animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-import Image from "next/image";
-import { TextRevealCardPreview } from "./ui/text-reveal-preview";
+"use client";
+
+import Lottie from "lottie-react";
+import animationData from "../json/bodybuilder-animation.json";
 
 export default function Home() {
   return (
-    <main className=" items-start preview flex min-h-screen w-full  items-center justify-center sm:p-10 p-2">
-      <TextRevealCardPreview />
+    <main className="flex min-h-screen items-center justify-center sm:p-10 p-2">
+      <div className="text-center space-y-4 sm:flex sm:items-center sm:space-x-4">
+        <div className="sm:w-1/2">
+          <h1 className="font-geistSans leading-tight text-gradient text-4xl sm:text-7xl font-normal  relative bg-clip-text bg-gradient-to-b from-neutral-100 to-neutral-300  py-8">
+            SUPERDANKO WEB SHOP
+          </h1>
+          <p className="font-normal text-xs md:text-xl text-neutral-400  max-w-lg mx-auto">
+            Please wait, we're building up!
+          </p>
+        </div>
+        <div className="sm:w-1/2">
+          <Lottie animationData={animationData} />
+        </div>
+      </div>
     </main>
   );
 }
