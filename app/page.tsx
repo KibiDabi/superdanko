9baddca1d2348678e51e253e1062c0787fc93b8d
Samuel Ljevar <s_lj_91@yahoo.com>
2024-03-22 09:20:41 +0100

    icons moved to the center

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,8 @@ export default function Home() {
             </div>
             
           </div>
-          <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 space-x-4 items-center justify-center mt-4">
+          {/* <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 space-x-4 items-center justify-center mt-4 sm:mt-2"></div> */}
+          <div className=" transform  space-x-4 items-center justify-center mt-4 sm:mt-2">
               <a
                 href="https://www.instagram.com/super_danko/"
                 className="group text-gray-600 hover:text-gray-300"
